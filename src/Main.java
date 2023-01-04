2b6c16f1e09976e2fa99df587cf6f9b12f347fe2
slet <slet113@mail.ru>
2023-01-04 17:33:27 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 2500;
+        int oneMilePerRubles = 20;
+        int amountMiles = ticketPrice / oneMilePerRubles;
+
+        System.out.println((amountMiles) + " миль начислено за купленный билет");
+    }
+}
